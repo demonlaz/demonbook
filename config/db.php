4d70efb1fb5ea492dcacbd2e0b2633aa6bf2cf4f
demonlaz <demon-l_91@mail.ru>
2017-08-01 20:47:39 +0300

    Update db.php
    заменил сервер

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=bookbdyii2',
+    'dsn' => 'mysql:host=databases.000webhost.com;dbname=bookbdyii2',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
